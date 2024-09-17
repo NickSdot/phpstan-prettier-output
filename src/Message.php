51ee1865ab03530bb1907ba544c6d42f2ae0be25
NickSdot <catch.github.com.nicksdot@nicksdot.dev>
2024-09-17 19:42:48 +0800

    refactor: introduced generic message regex pattern

--- src/Message.php
+++ src/Message.php
@@ -6,9 +6,7 @@ namespace NickSdot\PrettierStanOutput;
 
 use PHPStan\Analyser\Error;
 
-use function array_shift;
 use function count;
-use function mb_substr_count;
 use function preg_match;
 use function preg_quote;
 use function str_replace;
@@ -18,29 +16,60 @@ use function mb_strlen;
 use function str_repeat;
 use function array_splice;
 use function htmlspecialchars;
+use function max;
+use function str_ends_with;
+
+use const ENT_COMPAT;
+use const ENT_XML1;
 
 final readonly class Message
 {
     private const array PATTERN = [
-        'Method %s::%s() should return %%s but returns %%s.' => 'Method %s::%s() return type mismatch' . PHP_EOL . '⁉️ %s' . PHP_EOL . '⁉️ %s' . PHP_EOL,
-        'PHPDoc tag @return with type %s is incompatible with native type %s.' => 'PHPDoc tag @return is incompatible' . PHP_EOL . '⁉️ %s' . PHP_EOL . '⁉️ native type %s' . PHP_EOL,
-        'Parameter %%s %s of method %s expects %s, %s given.' => 'Parameter %s %s of method %s does not match' . PHP_EOL . '✅️ %s (expected)' . PHP_EOL . '❌️ %s (current)' . PHP_EOL,
+
+        // simple
+        'return %s but returns %s.' => 'return 1 but returns 2.' . PHP_EOL . PHP_EOL . '   1) %s' . PHP_EOL . '   2) %s' . PHP_EOL,
+        'type %s is incompatible with native type %s.' => 'type 1 is incompatible with native type 2.' . PHP_EOL . PHP_EOL . '   1) %s' . PHP_EOL . '   2) %s' . PHP_EOL,
+        'expects %s, %s given.' => 'expects 1, 2 given.' . PHP_EOL . PHP_EOL . '   1) %s' . PHP_EOL . '   2) %s' . PHP_EOL,
+        'type %s, %s given.' => 'type 1, 2 given.' . PHP_EOL . PHP_EOL . '   1) %s' . PHP_EOL . '   2) %s' . PHP_EOL,
+
+        // comparison
+        'between %s and trait %s will' => 'between 1 and trait 2 will %3$s' . PHP_EOL . PHP_EOL . '   1) %1$s' . PHP_EOL . '   2) %2$s' . PHP_EOL,
+        'between %s and %s will' => 'between 1 and 2 will %3$s' . PHP_EOL . PHP_EOL . '   1) %1$s' . PHP_EOL . '   2) %2$s' . PHP_EOL,
+        'between %s and %s is' => 'between 1 and 2 is %3$s' . PHP_EOL . PHP_EOL . '   1) %1$s' . PHP_EOL . '   2) %2$s' . PHP_EOL,
+        'between %s and %s results' => 'between 1 and 2 results  %3$s' . PHP_EOL . PHP_EOL . '   1) %1$s' . PHP_EOL . '   2) %2$s' . PHP_EOL,
+
+        // docs
+        'PHPDoc type %s of %%s PHPDoc type %s of %%s.' => 'PHPDoc type 1 of %2$s PHPDoc type 2 of %4$s' . PHP_EOL . PHP_EOL . '   1) %1$s' . PHP_EOL . '   2) %3$s' . PHP_EOL,
+
+        // variants
+        '%%s (%s) %%s should be covariant %%s (%s)%%%s' => '%1$s (1) %3$s should be covariant %4$s (2) %6$s' . PHP_EOL . PHP_EOL . '   1) %2$s' . PHP_EOL . '   2) %5$s' . PHP_EOL,
+        '%%s (%s) %%s should be contravariant %%s (%s)%%%s' => '%1$s (1) %3$s should be contravariant %4$s (2) %6$s' . PHP_EOL . PHP_EOL . '   1) %2$s' . PHP_EOL . '   2) %5$s' . PHP_EOL,
     ];
 
     public static function applyCustomFormatting(string $message): string
     {
         foreach (self::PATTERN as $oldPattern => $newPattern) {
 
-            $regex = '/' . str_replace([ '%%s', '%s' ], "([^']+?)", preg_quote($oldPattern, '/')) . '/';
+            $matchPlaceholders = str_replace(
+                [ '%%%s', '%%s', '%s' ],
+                [ "\s*(.+?)$", "(.+?)", "([^']+?)" ],
+                preg_quote($oldPattern, '/')
+            );
+
+            // open end strings without specified end will
+            // match the rest until the end of the string.
+
+            $regex = true === str_ends_with($oldPattern, '.') || true === str_ends_with($oldPattern, '%%%s')
+                ? '/' . $matchPlaceholders . '/'
+                : '/' . $matchPlaceholders . '\s*(.+)/';
 
             if (false !== preg_match($regex, $message, $matches)) {
 
-                array_shift($matches); // Remove the full match from the matches array
-
-                if (mb_substr_count($newPattern, '%s') === count($matches)) {
-                    $message = vsprintf($newPattern, $matches);
-                    break;
+                if (0 === count($matches)) {
+                    continue;
                 }
+
+                $message = str_replace($matches[0], vsprintf($newPattern, array_splice($matches, 1)), $message);
             }
         }
 
